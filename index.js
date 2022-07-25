d4b1e0b0cffc1d8b9480144285d89a318f5b1707
Darwin <serocefinodarwin@gmail.com>
2022-07-25 22:53:50 +0800

    add router email to server

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
+const emailRouter = require('./router')
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
+
+app.use(emailRouter)
 
 app.get('/',(req,res) => {
     res.send('Hello Worsld')
